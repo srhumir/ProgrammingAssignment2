232790d4d17377d32247a4f6465040b02fdc17f5
Reza <srh.umir@gmail.com>
2016-01-26 10:48:26 +0100

    I entered my code for assignment2 of  R Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The fillowing to functions save a matrix then cach the inverse of that 
+## matrix the first time the in inverse is caclulated. From then if the 
+## user try to compute the inverse again the cached inverse will be 
+##returned without computation.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Create a list that cach the matrix and its inverse
+makeCacheMatrix <- function(A, ...){
+       I <- NULL
+       set <- function(y){
+              A <<- y
+              I <<- NULL
+       }
+       get <- function() A
+       setinverse <- function(Inverse) I <<- Inverse
+       getinverse <- function() I
+       list(get = get, set = set, getinverse = getinverse, setinverse = setinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Get the output of abive function and compute the inverse of the matrix
+#if necessary.
+cacheSolve <- function(a, ...){
+       i <- a$getinverse()
+       if (!is.null(i)){
+              print("Cached inverse in returned")
+              return(i)
+       }
+       data <- a$get()
+       i <- solve(data)
+       a$setinverse(i)
+       i
 }
